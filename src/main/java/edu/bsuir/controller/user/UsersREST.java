8c6a270f340df81434eb7115b51196f4088eea21
Maksim Shevyakov <shevyakovmmbsuir@gmail.com>
2018-03-21 17:25:28 +0300

    UsersRest controller updated.

--- src/main/java/edu/bsuir/controller/user/UsersREST.java
+++ src/main/java/edu/bsuir/controller/user/UsersREST.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class UsersRESTController {
+public class UsersREST {
 
     @Autowired
     private UsersServiceImpl usersService;
